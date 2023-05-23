1b901d20aaa8f7e1abc1de3baf0f001dd08cafa4
Edras <monterrosoedras@gmail.com>
2023-05-22 23:27:26 -0600

    Adding C++ recognizer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
+const tokens = {
+  int: 'reservada',
+  string: 'reservada',
+  float: 'reservada',
+  bool: 'reservada',
+  if: 'reservada',
+  else: 'reservada',
+  for: 'reservada',
+  switch: 'reservada',
+  case: 'reservada',
+  break: 'reservada',
+  default: 'reservada',
+  '+': 'simbolo',
+  '-': 'simbolo',
+  '*': 'simbolo',
+  '/': 'simbolo',
+  '=': 'simbolo',
+  '<': 'simbolo',
+  '>': 'simbolo',
+  '>=': 'simbolo',
+  '<=': 'simbolo',
+  '==': 'simbolo',
+  ';': 'simbolo',
+}
+
+const ignore = {
+  '(': ' ',
+  ')': ' ',
+  '{': ' ',
+  '}': ' ',
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'validador-cPlusPlus';
+export class AppComponent implements OnInit {
+  title = 'validador-cPlusPlus'
+  reconocedor!: []
+  formValidator!: FormGroup
+  @ViewChild('result') result!: ElementRef
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formValidator = this.fb.group({
+      validator: ['', Validators.required],
+    })
+  }
+
+  submitValidator() {
+    if (this.formValidator.valid) {
+      this.verifyLexico(this.formValidator.get('validator')?.value)
+    }
+  }
+
+  verifyLexico(value: string): void {
+    const verifyNumber = /^([0-9])*$/
+    const token = value
+      .split(/\s+/)
+      .filter((t) => t.length > 0)
+      .map((t) => {
+        if (ignore[t as keyof typeof ignore]) return ''
+        if (verifyNumber.test(t)) return 'numero'
+        if (t.startsWith('"')) return 'valor'
+        return tokens[t as keyof typeof tokens]
+          ? tokens[t as keyof typeof tokens]
+          : 'identificador'
+      })
+
+    this.setResult(token)
+  }
+
+  setResult(token: any[]) {
+    let text = ''
+    token.forEach((t) => {
+      text += t + ' '
+    })
+
+    this.result.nativeElement.innerText = text
+    console.log(this.result.nativeElement)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AngularSplitModule } from 'angular-split'
+import { AngularMaterialModule } from './angular-material/angular-material.module'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularSplitModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
